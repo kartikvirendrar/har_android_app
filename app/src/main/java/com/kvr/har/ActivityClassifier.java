7c79284659013682d19527df5792e492f091ad91
Kartik Virendra Rajput <kartik.rajput20@vit.edu>
2022-04-28 15:05:10 +0530

    v2.0

---
+++ app/src/main/java/com/kvr/har/ActivityClassifier.java
@@ -0,0 +1,33 @@
+package com.kvr.har;
+
+import android.content.Context;
+
+import org.tensorflow.contrib.android.TensorFlowInferenceInterface;
+
+public class ActivityClassifier {
+
+    static {
+        System.loadLibrary("tensorflow_inference");
+    }
+
+    private TensorFlowInferenceInterface inferenceInterface;
+    private static final String MODEL_FILE="model.pb";
+    private static final String INPUT_NODE="lstm_1_input";
+    private static final String[] OUTPUT_NODES={"output/Softmax"};
+    private static final String OUTPUT_NODE="output/Softmax";
+    private static final long[] INPUT_SIZE={1,100,6};
+    private static final int OUTPUT_SIZE = 3;
+
+    public ActivityClassifier(Context context){
+        inferenceInterface=new TensorFlowInferenceInterface(context.getAssets(), MODEL_FILE);
+    }
+
+    public float[] predictProbablities(float[] data){
+        float[] result=new float[OUTPUT_SIZE];
+        inferenceInterface.feed(INPUT_NODE, data, INPUT_SIZE);
+        inferenceInterface.run(OUTPUT_NODES);
+        inferenceInterface.fetch(OUTPUT_NODE, result);
+        return result;
+    }
+
+}
