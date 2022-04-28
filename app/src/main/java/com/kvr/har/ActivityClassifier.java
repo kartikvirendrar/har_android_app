cedd1031769f3e98056899284106f9e7106522da
Kartik Virendra Rajput <kartik.rajput20@vit.edu>
2022-04-28 19:44:06 +0530

    v3.0

--- app/src/main/java/com/kvr/har/ActivityClassifier.java
+++ app/src/main/java/com/kvr/har/ActivityClassifier.java
@@ -6,23 +6,24 @@ import org.tensorflow.contrib.android.TensorFlowInferenceInterface;
 
 public class ActivityClassifier {
 
+    private static final String MODEL_FILE="model.pb";
+    private static final String INPUT_NODE="lstm_1_input";
+    private static final String[] OUTPUT_NODES={"dense2/Softmax"};
+    private static final String OUTPUT_NODE="dense2/Softmax";
+    private static final long[] INPUT_SIZE={1,100,6};
+    private static final int OUTPUT_SIZE = 3;
+
     static {
         System.loadLibrary("tensorflow_inference");
     }
 
-    private TensorFlowInferenceInterface inferenceInterface;
-    private static final String MODEL_FILE="model.pb";
-    private static final String INPUT_NODE="lstm_1_input";
-    private static final String[] OUTPUT_NODES={"output/Softmax"};
-    private static final String OUTPUT_NODE="output/Softmax";
-    private static final long[] INPUT_SIZE={1,100,6};
-    private static final int OUTPUT_SIZE = 3;
+    private final TensorFlowInferenceInterface inferenceInterface;
 
     public ActivityClassifier(Context context){
         inferenceInterface=new TensorFlowInferenceInterface(context.getAssets(), MODEL_FILE);
     }
 
-    public float[] predictProbablities(float[] data){
+    public float[] predictProbabilities(float[] data){
         float[] result=new float[OUTPUT_SIZE];
         inferenceInterface.feed(INPUT_NODE, data, INPUT_SIZE);
         inferenceInterface.run(OUTPUT_NODES);
--- app/src/main/java/com/kvr/har/MainActivity.java
+++ app/src/main/java/com/kvr/har/MainActivity.java
@@ -2,6 +2,7 @@ package com.kvr.har;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -10,26 +11,24 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import static android.content.ContentValues.TAG;
-
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
+    private static final int TIME_STAMP = 100;
+    private static final String TAG = "MainActivity";
 
-    private static final int TIME_STAMP=100;
     private static List<Float> ax,ay,az;
     private static List<Float> gx,gy,gz;
 
     private SensorManager mSensorManager;
     private Sensor mAccelerometer, mGyroscope;
 
-    private float[] results;
     private ActivityClassifier classifier;
 
     private TextView sittingTV, standingTV, lyingTV;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,13 +40,14 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         ax=new ArrayList<>(); ay=new ArrayList<>(); az=new ArrayList<>();
         gx=new ArrayList<>(); gy=new ArrayList<>(); gz=new ArrayList<>();
 
-        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mSensorManager=(SensorManager) getSystemService(SENSOR_SERVICE);
+        mAccelerometer=mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         mGyroscope = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
 
-        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_FASTEST);
-        mSensorManager.registerListener(this, mGyroscope, SensorManager.SENSOR_DELAY_FASTEST);
+        classifier=new ActivityClassifier(getApplicationContext());
 
+        mSensorManager.registerListener(this,mAccelerometer, SensorManager.SENSOR_DELAY_FASTEST);
+        mSensorManager.registerListener(this,mGyroscope, SensorManager.SENSOR_DELAY_FASTEST);
     }
 
     private void initLayoutItems(){
@@ -59,16 +59,16 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     @Override
     public void onSensorChanged(SensorEvent event) {
         Sensor sensor = event.sensor;
-        if (sensor.getType() == Sensor.TYPE_ACCELEROMETER){
+        if (sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
             ax.add(event.values[0]);
             ay.add(event.values[1]);
             az.add(event.values[2]);
-        }
-        if (sensor.getType() == Sensor.TYPE_GYROSCOPE){
+        } else {
             gx.add(event.values[0]);
             gy.add(event.values[1]);
             gz.add(event.values[2]);
         }
+
         predictActivity();
     }
 
@@ -77,23 +77,25 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     }
 
-    private void predictActivity(){
+    @SuppressLint("SetTextI18n")
+    private void predictActivity() {
         List<Float> data=new ArrayList<>();
         if (ax.size() >= TIME_STAMP && ay.size() >= TIME_STAMP && az.size() >= TIME_STAMP
-            && gx.size() >= TIME_STAMP && gy.size() >= TIME_STAMP && gz.size() >= TIME_STAMP){
+                && gx.size() >= TIME_STAMP && gy.size() >= TIME_STAMP && gz.size() >= TIME_STAMP) {
             data.addAll(ax.subList(0,TIME_STAMP));
             data.addAll(ay.subList(0,TIME_STAMP));
             data.addAll(az.subList(0,TIME_STAMP));
+
             data.addAll(gx.subList(0,TIME_STAMP));
             data.addAll(gy.subList(0,TIME_STAMP));
             data.addAll(gz.subList(0,TIME_STAMP));
 
-            results = classifier.predictProbablities(toFloatArray(data));
+            float[] results = classifier.predictProbabilities(toFloatArray(data));
             Log.i(TAG, "predictActivity: "+ Arrays.toString(results));
 
-            sittingTV.setText("SITTING: \t"+ results[0]);
-            standingTV.setText("STANDING: \t"+ results[1]);
-            lyingTV.setText("LYING: \t"+ results[2]);
+            sittingTV.setText("SITTING: \t"+ round(results[0]));
+            standingTV.setText("STANDING: \t"+ round(results[1]));
+            lyingTV.setText("LYING: \t"+ round(results[2]));
 
             data.clear();
             ax.clear(); ay.clear(); az.clear();
@@ -101,11 +103,17 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         }
     }
 
-    private float[] toFloatArray(List<Float> data){
+    private float round(float value) {
+        BigDecimal bigDecimal=new BigDecimal(Float.toString(value));
+        bigDecimal = bigDecimal.setScale(2, BigDecimal.ROUND_HALF_UP);
+        return bigDecimal.floatValue();
+    }
+
+    private float[] toFloatArray(List<Float> data) {
         int i=0;
         float[] array=new float[data.size()];
-        for (Float f:data){
-            array[i++] = (f!=null ? f : Float.NaN);
+        for (Float f:data) {
+            array[i++] = (f != null ? f: Float.NaN);
         }
         return array;
     }
@@ -113,8 +121,8 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     @Override
     protected void onResume() {
         super.onResume();
-        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_FASTEST);
-        mSensorManager.registerListener(this, mGyroscope, SensorManager.SENSOR_DELAY_FASTEST);
+        mSensorManager.registerListener(this,mAccelerometer, SensorManager.SENSOR_DELAY_FASTEST);
+        mSensorManager.registerListener(this,mGyroscope, SensorManager.SENSOR_DELAY_FASTEST);
     }
 
     @Override
