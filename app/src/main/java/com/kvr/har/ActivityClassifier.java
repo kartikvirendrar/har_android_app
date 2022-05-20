855a85483e3a2b28206f11fe813fc8f6a5729274
Kartik Virendra Rajput <kartik.rajput20@vit.edu>
2022-05-20 13:37:43 +0530

    v3.0

--- app/src/main/java/com/kvr/har/ActivityClassifier.java
+++ app/src/main/java/com/kvr/har/ActivityClassifier.java
@@ -6,25 +6,25 @@ import org.tensorflow.contrib.android.TensorFlowInferenceInterface;
 
 public class ActivityClassifier {
 
-    private static final String MODEL_FILE="model.pb";
+    private static final String MODEL_FILE="orginal_model.pb";
     private static final String INPUT_NODE="lstm_1_input";
-    private static final String[] OUTPUT_NODES={"dense2/Softmax"};
-    private static final String OUTPUT_NODE="dense2/Softmax";
-    private static final long[] INPUT_SIZE={1,100,6};
-    private static final int OUTPUT_SIZE = 3;
+    private static final String[] OUTPUT_NODES={"output/Softmax"};
+    private static final String OUTPUT_NODE="output/Softmax";
+    private static final long[] INPUT_SIZE={1,100,9};
+    private static final int OUTPUT_SIZE = 7;
 
     static {
         System.loadLibrary("tensorflow_inference");
     }
 
-    private final TensorFlowInferenceInterface inferenceInterface;
+    private TensorFlowInferenceInterface inferenceInterface;
 
-    public ActivityClassifier(Context context){
-        inferenceInterface=new TensorFlowInferenceInterface(context.getAssets(), MODEL_FILE);
+    public ActivityClassifier(Context context) {
+        inferenceInterface = new TensorFlowInferenceInterface(context.getAssets(), MODEL_FILE);
     }
 
-    public float[] predictProbabilities(float[] data){
-        float[] result=new float[OUTPUT_SIZE];
+    public float[] predictProbabilities(float[] data) {
+        float[] result = new float[OUTPUT_SIZE];
         inferenceInterface.feed(INPUT_NODE, data, INPUT_SIZE);
         inferenceInterface.run(OUTPUT_NODES);
         inferenceInterface.fetch(OUTPUT_NODE, result);
